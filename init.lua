17a62742b33182ddda8b0e8017ebe15ce5d0abf2
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2025-12-28 20:15:16 +0300

    feat: added new plugins and configs

--- init.lua
+++ init.lua
@@ -1,14 +1,11 @@
 vim.opt.rtp:remove("/usr/share/vim/vimfiles")
 vim.cmd("packadd nohlsearch")
-vim.cmd(":hi Statusline guibg=NONE")
-
 vim.o.relativenumber = true
 vim.o.number = true
 vim.o.swapfile = false
 vim.o.tabstop = 4
-vim.g.mapleader = "'"
+vim.o.incsearch = true
 
-vim.keymap.set('n', '<C-n>', ':Ntree<CR>')
 vim.keymap.set({'n', 'v'}, 'H', '0')
 vim.keymap.set({'n', 'v'}, 'L', '$')
 vim.keymap.set('n', 'gb', '<C-^>')
@@ -16,13 +13,27 @@ vim.keymap.set('v', '<C-y>', '"*y')
 
 vim.pack.add{
   { src =  'https://github.com/chomosuke/typst-preview.nvim' },
-  { src =  'https://github.com/mason-org/mason.nvim' },
   { src =  'https://github.com/nvim-mini/mini.pick' },
+  { src =  'https://github.com/stevearc/oil.nvim' },
 }
 
-require("mason").setup()
-typst = require('typst-preview')
+require("oil").setup()
+vim.keymap.set('n', '<C-n>', ':Oil<CR>')
 
-mini = require("mini.pick").setup()
-vim.keymap.set('n', '<leader>r', function() MiniPick.builtin.grep_live({"rg"}) end )
-vim.keymap.set('n', '<leader>f', function() MiniPick.builtin.files() end )
+vim.api.nvim_create_autocmd('LspAttach', {
+		callback = function(x)
+				local client = vim.lsp.get_client_by_id(x.data.client_id)
+				if client:supports_method('textDocument/completion') then
+						vim.lsp.completion.enable(true, client.id, x.buf, {autotrigger = true})
+				end
+		end
+})
+vim.cmd("set completeopt+=noselect")
+
+vim.lsp.config['tinymist'] = { cmd = {'tinymist'} }
+vim.lsp.enable({'tinymist'})
+
+mini = require("mini.pick").setup({ mappings = {move_down = '<C-j>', move_up = '<C-k>'} })
+vim.keymap.set('n', '\'r', function() MiniPick.builtin.grep_live({"rg"}) end )
+vim.keymap.set('n', '\'f', function() MiniPick.builtin.files() end )
+vim.keymap.set('n', '\'h', function() MiniPick.builtin.help() end )
