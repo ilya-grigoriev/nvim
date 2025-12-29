3765626c362c691f8f8bea07987275c4ac675f36
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2025-12-29 18:34:20 +0300

    fix: added actioning of keymaps to Select mode

--- init.lua
+++ init.lua
@@ -31,7 +31,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 vim.lsp.config['tinymist'] = { cmd = {'tinymist'} }
 vim.lsp.enable({'tinymist'})
 
-mini = require("mini.pick").setup({ mappings = {move_down = '<C-j>', move_up = '<C-k>', caret_left = '<C-h>', caret_right = '<C-l>'} })
+mini = require("mini.pick").setup({ mappings = {caret_left = '<C-h>', caret_right = '<C-l>'} })
 vim.keymap.set('n', '\'r', function() MiniPick.builtin.grep_live() end )
 vim.keymap.set('n', '\'f', function() MiniPick.builtin.files() end )
 vim.keymap.set('n', '\'h', function() MiniPick.builtin.help() end )
@@ -39,5 +39,5 @@ vim.keymap.set('n', '\'h', function() MiniPick.builtin.help() end )
 require("luasnip").setup({enable_autosnippets = true})
 require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/snippets/"})
 vim.keymap.set('i', '<Tab>', function() require("luasnip").expand() end, {silent = true})
-vim.keymap.set('i', '<C-j>', function() require("luasnip").jump(1) end, {silent = true})
-vim.keymap.set('i', '<C-k>', function() require("luasnip").jump(-1) end, {silent = true})
+vim.keymap.set({'i', 's'}, '<C-j>', function() require("luasnip").jump(1) end, {silent = true})
+vim.keymap.set({'i', 's'}, '<C-k>', function() require("luasnip").jump(-1) end, {silent = true})
