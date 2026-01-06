4c4570c03997aabfa25a9e0a125686386eb83a84
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2026-01-06 12:17:11 +0300

    feat(snippets)

--- snippets/c.lua
+++ snippets/c.lua
@@ -1,3 +1,5 @@
 return {
 		s("inc", 	{ t"#include <", i(1), t">", i(2) }),
+		s("main", 	{ t({"int main(int argc, char *argv[])", "{", "\t"}), i(1), t({"", "}"}) }),
+		s("pr",		{ t'printf("%', i(1), t'", ', i(2), t');' }),
 }
