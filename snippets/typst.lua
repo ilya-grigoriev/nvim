bfde6674df92409e34c753fa327d5838cfbe81b2
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2026-01-26 21:14:39 +0300

    feat(snippets:typst)

--- snippets/typst.lua
+++ snippets/typst.lua
@@ -3,10 +3,12 @@ return {
 	s("it", 	{ t"_", i(1), t"_", i(2) }),
 	s("d", 		{ t"upright(", i(1), t")", i(2) }),
 	s("pb", 	{ t{"#pagebreak()", ""}, i(1) }),
+	s("fr", 	{ t'(', i(1), t')/(', i(2), t')' }),
 
 	s("mt", 	{ t'op("', i(1), t'")', i(2) }),
 	s("m", 		{ t'$', i(1), t'$', i(2) }),
 	s("mm",		{ t'$ ', i(1), t' $', i(2) }),
+	s("lm", 	{ i(1), t' = #let ', i(2), t'=', rep(1), t'; ', i(3), t' #', rep(2) }),
 
 	s("img",	{ t({"#align(", "\tcenter", '\tfigure(image("'}), i(1), t({'"))', ')'}), i(2) }),
 	s("le",		{ t"#let ", i(1), t(' = '), i(2) }),
